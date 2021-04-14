3ceb79f842a08a5e7dc90072eb67565408a38748
Nikita Parmon <nikitka.parmon@mail.ru>
2021-04-14 09:22:30 +0300

    laba 6 complete

--- scripts/laba6/day_course.php
+++ scripts/laba6/day_course.php
@@ -1,37 +1,56 @@
 <?php
 
 
-$currDate= date("Y-n-j");
+$currDate = date("Y-n-j");
+$yesterdayDate=date('Y-n-j', strtotime($currDate. " - 1 day"));
+
+$arr = getCourseArr($yesterdayDate);
+echo getCourseBlock($yesterdayDate, $arr);
 
 $arr = getCourseArr($currDate);
+echo getCourseBlock($currDate, $arr);
 
-//echo getCourseBlock($currDate);
 
-function getCourseBlock($date,$course){
 
-   $block='<div class="day_course">';
-   $block.="<h1>$date</h1>";
-   require("test.php");
+function getCourseBlock($date, $course)
+{
 
-   $block.="</div>";
-   return $block;
+    $block = '<div class="day_course">';
+    $block .= "<h1>$date</h1>";
+    $block .= getCourseBlockFromCourseArr($course, 4);
+    $block .= getCourseBlockFromCourseArr($course, 5);
+    $block .= getCourseBlockFromCourseArr($course, 16);
+    $block .= "</div>";
+    return $block;
 }
 
-function getCourseArr($date){
+function getCourseArr($date)
+{
 
-    $url="https://www.nbrb.by/api/exrates/rates?ondate=".$date."&periodicity=0";
+    $url = "https://www.nbrb.by/api/exrates/rates?ondate=" . $date . "&periodicity=0";
     $curl = curl_init($url);
 
-    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 
-    $out=curl_exec($curl);
-    if(curl_error($curl)) {
+    $out = curl_exec($curl);
+    if (curl_error($curl)) {
         return null;
     }
 
     curl_close($curl);
-    $out=json_decode($out);
+    $out = json_decode($out);
     return $out;
 
+}
+
+function getCourseBlockFromCourseArr($course, $i)
+{
+    $block = '<div class="Currency_block">';
+    $currencyName = $course[$i]->Cur_Abbreviation;
+    $currencyVal = $course[$i]->Cur_OfficialRate;
+    $block .= "<p>$currencyName  $currencyVal  </p>";
+
+    $block .= "</div>";
+    return $block;
 }
